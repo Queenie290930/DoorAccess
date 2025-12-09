3a2293718a7d6dcff9e32161dd62f24f18c9e040
Queenie290930 <queenie.joy@hotmail.se>
2025-12-09 18:39:14 +0100

    Delete DoorAcces directory

--- DoorAcces/SafeInput.h
+++
@@ -1,8 +0,0 @@
-#ifndef SAFEINPUT_H
-#define SAFEINPUT_H
-
-void safeInput(char *buffer, int maxSize);
-int readInt(const char *prompt);
-void readString(const char *prompt, char *buffer, int maxSize);
-
-#endif
